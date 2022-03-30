5817e26a7c7fbb69b7ae2a211060733a8ba3d07e
fishyer <630709658@qq.com>
2022-03-31 00:29:40 +0800

    修改var为const

--- preload.js
+++ preload.js
@@ -5,85 +5,101 @@ const clip = require("electron").clipboard;
 // const nativeImage = require('electron').nativeImage
 // const clipboardListener = require("clipboard-event");
 
-var path = require('path');
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+
 // const fs = require('fs-extra');
 
-let tabListCache = []
+// let tabListCache = []
 
-var dir = "/Users/yutianran/Documents/Test/"
+const mdHost = "https://filedn.com/lCwzHP6EMjTBdpWjo6o7aHy/"
+// const dir = "/Users/yutianran/Documents/Test/"
+const home = window.utools.getPath("home")
+const dir=path.join(home, '/Dropbox/MyBillfish/Public Folder');
+let folder = path.resolve(__dirname);
 
 window.exports = {
     'hello': {
-        mode: 'list',
+        mode: 'none',
         args: {
-            enter: (action, callbackSetList) => {
+            enter: (action) => {
                 console.log("##1")
-                getClipboardData(callbackSetList);
+                console.log("folder=" + folder)
+                console.log("home=" + home)
+                console.log("dir=" + dir)
+                const clipboardData = getClipboardData();
+                const mdlink = withUrlEncode(clipboardData);
+                window.utools.copyText(mdlink);
+                // window.utools.outPlugin()
+                // window.utools.hideMainWindow()
             },
-            search: (action, searchWord, callbackSetList) => {
-                console.log("##2")
-                const folder = path.resolve(__dirname);
-                console.log("folder="+folder)
-                getClipboardData(callbackSetList);
-                // var clipboardData = clip.readText()
-                // console.log("##2-" + clipboardData)
-                // if (!searchWord) return callbackSetList(tabListCache)
-                // console.log(searchWord)
-                // var urlEncode = encodeURIComponent(searchWord)
-                // var urlDecode = decodeURIComponent(urlEncode)
-                // tabListCache = []
-                // tabListCache.push({
-                //     title: "clipboardData",
-                //     description: clipboardData
-                // })
-                // tabListCache.push({
-                //     title: searchWord,
-                //     description: "![](https://filedn.com/lCwzHP6EMjTBdpWjo6o7aHy/" + urlEncode + ")"
-                // })
-                // callbackSetList(tabListCache)
-            },
-            select: (action, itemData) => {
-                console.log("##3")
-                // console.log(itemData.description)
-            }
+            // search: (action, searchWord, callbackSetList) => {
+            //     console.log("##2")
+            //     getClipboardData(callbackSetList);
+            //     // var clipboardData = clip.readText()
+            //     // console.log("##2-" + clipboardData)
+            //     // if (!searchWord) return callbackSetLis1t(tabListCache)
+            //     // console.log(searchWord)1 
+            //     // var urlEncode = encodeURIComponent(searchWord)
+            //     // var urlDecode = decodeURIComponent(urlEncode)
+            //     // tabListCache = []
+            //     // tabListCache.push({
+            //     //     title: "clipboardData",
+            //     //     description: clipboardData
+            //     // })
+            //     // tabListCache.push({
+            //     //     title: searchWord,
+            //     //     description: "![](https://filedn.com/lCwzHP6EMjTBdpWjo6o7aHy/" + urlEncode + ")"
+            //     // })
+            //     // callbackSetList(tabListCache)
+            // },
+            // select: (action, itemData) => {
+            //     console.log("##3")
+            //     // console.log(itemData.description)
+            // }
         }
     },
 }
 
-function getClipboardData(callbackSetList) {
-    tabListCache = []
+function getClipboardData() {
+    // tabListCache = []
     // 文本类型
     if (clip.readText() !== "") {
         var clipboardData = clip.readText();
         console.log("文本类型：" + clipboardData);
-        var urlEncode = encodeURIComponent(clipboardData);
-        var mdlink = `![](https://filedn.com/lCwzHP6EMjTBdpWjo6o7aHy/${urlEncode}`;
-        window.utools.copyText(mdlink);
-        tabListCache.push({
-            title: clipboardData,
-            description: mdlink
-        });
+        // tabListCache.push({
+        //     title: clipboardData,
+        //     description: mdlink
+        // });
+        return clipboardData
     }
 
     // 图片类型
     else if (!clip.readImage().isEmpty()) {
         var clipboardData = clip.readImage();
-        console.log("图片类型：" + clipboardData.constructor);
+        // console.log("图片类型：" + clipboardData.constructor);
         const buffer = clipboardData.toPNG();
-        console.log("buffer:" + buffer.constructor);
+        // console.log("buffer:" + buffer.constructor);
         var imgData = new Buffer(clipboardData.toPNG(), 'base64'); //Buffer编码
         const fileName = `${Date.now()}_${Math.floor(Math.random() * 1000)}`;
         var imgPath = dir + fileName + ".png"
-        console.log("imgPath:" + imgPath);
+        // console.log("imgPath:" + imgPath);
         fs.writeFile(imgPath, imgData, function (err) {
             if (err) {
-                console.log("图片上传失败")
+                console.log("图片上传失败:" + imgPath)
                 console.log(err);
+                return ""
             } else {
-                console.log("图片保存成功：" + imgPath);
+                console.log("图片保存成功:" + imgPath);
                 // window.utools.hideMainWindow()
+
+
+                return imgPath
             }
+            // tabListCache.push({
+            //     title: copyedFiles[0].name,
+            //     description: imgPath
+            // });
         });
     }
 
@@ -92,14 +108,34 @@ function getClipboardData(callbackSetList) {
         console.log("文件类型：" + clipboardData);
         var copyedFiles = window.utools.getCopyedFiles();
         console.log(copyedFiles[0]);
-        tabListCache.push({
-            title: "文件类型：",
-            description: copyedFiles[0].name
-        });
+        // tabListCache.push({
+        //     title: "文件类型：",
+        //     description: copyedFiles[0].name
+        // });
+        imgName = copyedFiles[0].name
+        imgPath = copyedFiles[0].path
+        console.log("imgPath=" + imgPath)
+        destPath=path.join(dir,imgName)
+        console.log("destPath="+destPath)
+        // fs.rename(imgPath, destPath, function (err) {
+        //     if (err) throw err;
+        //     fs.stat(destPath, function (err, stats) {
+        //         if (err) throw err;
+        //         console.log('stats: ' + JSON.stringify(stats));
+        //     });
+        // });
+        return imgPath
         // TODO 对于md文件，可以先用pandoc在pcloud文件夹下面生成对应的html文件，就相当于发布博客了
         // TODO 对于图片，可以先压缩一下，然后复制到pcloud文件夹，就相当于图床了
     }
-    callbackSetList(tabListCache);
+    // callbackSetList(tabListCache);
+}
+
+
+function withUrlEncode(src) {
+    var urlEncode = encodeURIComponent(src);
+    var mdlink = `![](${mdHost}${urlEncode})`;
+    return mdlink;
 }
 
 // 保存剪切版里面的图片到本地文件夹
